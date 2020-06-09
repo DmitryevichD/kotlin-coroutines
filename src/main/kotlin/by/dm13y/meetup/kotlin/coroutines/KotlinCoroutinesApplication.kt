88ddf040e0f48d6a2b7bb36608927821b41301df
DmitrievichD <d.dmitrievich@itransition.com>
2020-06-09 23:42:34 +0300

    init project

---
+++ src/main/kotlin/by/dm13y/meetup/kotlin/coroutines/KotlinCoroutinesApplication.kt
@@ -0,0 +1,11 @@
+package by.dm13y.meetup.kotlin.coroutines
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCoroutinesApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCoroutinesApplication>(*args)
+}
---
+++ src/test/kotlin/by/dm13y/meetup/kotlin/coroutines/KotlinCoroutinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.dm13y.meetup.kotlin.coroutines
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCoroutinesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
